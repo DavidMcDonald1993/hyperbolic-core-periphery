1f24e64c3e3b5d35fbb668c7755f41b953641f9e
David McDonald <davemcdonald@live.co.uk>
2019-03-01 12:53:28 +0000

    chnaged python version for bluebear

--- run_competition.sh
+++ run_competition.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=runCompetition
 #SBATCH --output=runCompetition_%A_%a.out
 #SBATCH --error=runCompetition_%A_%a.err
-#SBATCH --array=0-2519
-#SBATCH --time=00:10:00
+#SBATCH --array=0-359
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=1G
+#SBATCH --mem=3G
 # SBATCH --mail-type ALL
 
 # DATA_DIR="/rds/homes/d/dxm237/data"
@@ -14,7 +14,7 @@
 ARR=("--seed="{0..29}" --exp="{one_core,two_core,two_core_with_residual}" --algorithm="{BE,divisive,KM_ER,KM_config})
 
 module purge; module load bluebear
-module load apps/python3/3.5.2
+module load Python/3.6.3-iomkl-2018a
 
 pip install --user cpalgorithm
 
