89f9c7f2443a68bb78a23097ab60db3d0b9ed6bd
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-04-10 13:46:57 +0100

    added test script for sbatch

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+slurm_options=$(echo \
+--qos=bbshort \
+--job-name=testJob \
+--time=01:00:00 \
+--mem=1G
+)
+
+modules=$(echo \
+module purge \
+module load bluebear \
+module load apps/python3/3.5.2
+)
+
+cmd="echo hello world"
+
+echo sbatch ${slurm_options} ${modules} ${cmd}
