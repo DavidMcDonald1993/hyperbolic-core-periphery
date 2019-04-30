583173d41006c5f9183cc7229424004c750e668d
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-04-30 16:07:49 +0100

    embedding grn scipt added

--- embed_grn.sh
+++ embed_grn.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-hrs=10
+days=3
+hrs=00
 mem=5G
 
 heat=/rds/projects/2018/hesz01/heat/main.py
-
 
 edgelist=edgelists/grn/edgelist.tsv
 embedding_dir=embeddings/grn/
 walks_dir=walks/grn/
 
-slurm_options=$(echo \
---job-name=performEmbeddingsGRN \
---time=${hrs}:00:00 \
---mem=${mem} \
---output=performEmbeddingsGRN.out \
---error=performEmbeddingsGRN.err
-)
+for dim in 05 10 25 50 100
+do
+	for seed in 00..29
+	do
 
-modules=$(echo \
-module purge\; \
-module load bluebear\; \
-module load apps/python3/3.5.2\; \
-module load apps/keras/2.0.8-python-3.5.2
-)
+		slurm_options=$(echo \
+		--job-name=performEmbeddingsGRN-${dim}-${seed} \
+		--time=${days}-${hrs}:00:00 \
+		--mem=${mem} \
+		--output=performEmbeddingsGRN-${dim}-${seed}.out \
+		--error=performEmbeddingsGRN-${dim}-${seed}.err
+		)
 
-cmd=$(echo python ${heat} --edgelist ${edgelist} \
---embedding ${embedding_dir} --walks ${walks_dir} --dim 10 -e 25 --sigma 1 --context-size 3 --directed --lr 3.)
+		modules=$(echo \
+		module purge\; \
+		module load bluebear\; \
+		module load apps/python3/3.5.2\; \
+		module load apps/keras/2.0.8-python-3.5.2\; \
+		)
 
-echo submitting ${cmd}
-sbatch ${slurm_options} <(echo -e '#!/bin/bash\n'${modules}'\n'${cmd})
+		cmd=$(echo python ${heat} --edgelist ${edgelist} \
+		--embedding ${embedding_dir} --walks ${walks_dir} --dim ${dim} -e 25 --directed --seed ${seed} )
+
+		echo submitting ${cmd}
+		sbatch ${slurm_options} <(echo -e '#!/bin/bash\n'${modules}'\n'${cmd})
+
+	done
+done
+
+
+
