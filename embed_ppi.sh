85fa83e0f5c212478a03f6957014e70961889003
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-05-02 12:33:07 +0100

    embed scipt added

---
+++ embed_ppi.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+days=3
+hrs=00
+mem=32G
+
+heat=/rds/projects/2018/hesz01/heat/main.py
+
+edgelist=edgelists/ppi/string_yeast_ppi.tsv
+embedding_dir=embeddings/ppi/
+walks_dir=walks/ppi/
+
+for dim in 25 50 100
+do
+	for seed in {00..5}
+	do
+
+		slurm_options=$(echo \
+		--job-name=performEmbeddingsPPI-${dim}-${seed} \
+		--time=${days}-${hrs}:00:00 \
+		--mem=${mem} \
+		--output=performEmbeddingsPPI-${dim}-${seed}.out \
+		--error=performEmbeddingsPPI-${dim}-${seed}.err
+		)
+
+		modules=$(echo \
+		module purge\; \
+		module load bluebear\; \
+		module load apps/python3/3.5.2\; \
+		module load apps/keras/2.0.8-python-3.5.2\; \
+		)
+
+		cmd=$(echo python ${heat} --edgelist ${edgelist} \
+		--embedding ${embedding_dir} --walks ${walks_dir} --dim ${dim} -e 25 --directed --seed ${seed} )
+
+		echo submitting ${cmd}
+		sbatch ${slurm_options} <(echo -e '#!/bin/bash\n'${modules}'\n'${cmd})
+
+	done
+done
+
+
+
